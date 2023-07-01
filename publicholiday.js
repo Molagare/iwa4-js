9f94c18fb776d24a205c41da06d9f4d83f851506
Molagare Modiba <molagare@gmail.com>
2023-07-01 11:45:02 +0200

    first commit. completed iwa04

---
+++ publicholiday.js
@@ -0,0 +1,39 @@
+const year = 2050;
+const status = 'student';
+let count = 0;
+
+if (year==2050){
+    console.log('January', 'New Years Day');
+    console.log('March', 'Human Rights Day')
+
+   let date = 'April'
+     console.log(date, 'Family Day');
+     console.log(date, 'Freedom day'); 
+     count = 4
+}
+
+if (status == 'student') {
+    console.log('June', 'Youth Day');
+    count += 1
+
+{
+    console.log('August', 'womens Day');
+    console.log('September', 'Heritage Day');}
+
+}
+let date = 'December';
+console.log(date, 'Day of Reconciliation')
+count += 3
+
+if (status == parent) {
+    console.log(ValentinesDay )}
+    else {
+        console.log(date, 'christmast Day');
+        count += 1
+        console.log(date, 'Day of Goodwill');
+        count += 1
+}
+{
+console.log('your status is:', status);
+console.log('the year is:', year);
+console.log('The total holidays is', count); }
